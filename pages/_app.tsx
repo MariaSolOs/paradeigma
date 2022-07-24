9a7bf8eb3e980f51fc1db84603295fcea29ca9c0
Maria José Solano <majosolano99@gmail.com>
2022-07-24 08:31:45 -0700

    Done with _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
-import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import createEmotionCache from 'styles/emotion-cache';
+import theme from 'styles/theme';
 
-function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+import type { AppProps as NextAppProps } from 'next/app';
+import type { EmotionCache } from '@emotion/react';
+
+const clientCache = createEmotionCache();
+
+type AppProps = NextAppProps & {
+    emotionCache?: EmotionCache;
+}
+
+const App = (props: AppProps) => {
+    const { Component, emotionCache = clientCache, pageProps } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                {/* This viewport tag needs to be here, not in _document.tsx */}
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component { ...pageProps } />
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
 
 export default App;
