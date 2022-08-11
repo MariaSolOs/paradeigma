5e5789815b596bca10060245ef5063da31fba199
Maria José Solano <majosolano99@gmail.com>
2022-08-10 22:37:46 -0700

    Remove html icon

--- lib/snippet.ts
+++ lib/snippet.ts
@@ -9,7 +9,6 @@ export const getLanguageIcon = (language: Language): string => {
     switch(language) {
         case 'csharp': return 'devicon-csharp-plain';
         case 'css': return 'devicon-css3-plain-wordmark';
-        case 'html': return 'devicon-html5-plain-wordmark';
         case 'java': return 'devicon-java-plain';
         case 'javascript': return 'devicon-javascript-plain';
         case 'markdown': return 'devicon-markdown-original';
