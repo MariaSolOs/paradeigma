11a7ead3951652dab0eb7f377cf0eaa2e667d499
Maria José Solano <majosolano99@gmail.com>
2022-08-17 22:42:52 -0700

    Clean up getHookSdk

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -7,8 +7,10 @@ import { getSdkWithHooks } from 'graphql-server/sdk';
 export const getHookedSdk = () => {
     // The root URL depends on whether we're calling this from the browser
     // or the backend.
-    const vercelUrl = process.env['VERCEL_URL'] || process.env['NEXT_PUBLIC_VERCEL_URL'];
-    console.log('VERCEL URL', vercelUrl); // TODO: Delete this when I make sure it works in production
-    const graphQLClient = new GraphQLClient(`${vercelUrl}/api/graphql`);
+    const env = (process.env['VERCEL_ENV'] || process.env['NEXT_PUBLIC_VERCEL_ENV'])!;
+    const protocolPrefix = env === 'development' ? 'http://' : 'https://';
+    const vercelUrl = (process.env['VERCEL_URL'] || process.env['NEXT_PUBLIC_VERCEL_URL'])!;
+
+    const graphQLClient = new GraphQLClient(`${protocolPrefix}${vercelUrl}/api/graphql`);
     return getSdkWithHooks(graphQLClient);
 }
