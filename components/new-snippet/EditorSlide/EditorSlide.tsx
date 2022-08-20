540ec4a88417d0650f8df462d9f757b63a8c16d5
Maria José Solano <majosolano99@gmail.com>
2022-08-20 14:19:28 -0700

    Allow editor slide to continue when in dev

--- components/new-snippet/EditorSlide/EditorSlide.tsx
+++ components/new-snippet/EditorSlide/EditorSlide.tsx
@@ -45,78 +45,82 @@ const AceEditor = dynamic(async () => {
 // Snippets should be short.
 const EDITOR_NUM_LINES = 20;
 
-const EditorSlide: FC<EditorSlideProps> = (props) => (
-    <>
-        <S.Slide>
-            <S.FieldsContainer>
-                <S.FormControl>
-                    <Input
-                    value={props.name}
-                    onChange={(event) => props.onNameChange(event.target.value)}
-                    placeholder="Baptize your snippet."
-                    required />
-                    <S.FormLabel sx={{ textIndent: '0.5rem' }}>
-                        <TungstenOutlinedIcon />{' '}
-                        Name it like a variable: Keep it short, but self-explanatory.
-                    </S.FormLabel>
-                </S.FormControl>
-                <S.FormControl>
-                    <Input
-                    value={props.description}
-                    onChange={event => props.onDescriptionChange(event.target.value)}
-                    placeholder="What is your snippet about?"
-                    multiline
-                    rows={3} />
-                    <S.FormLabel sx={{ textIndent: '0.5rem' }}>
-                        <TungstenOutlinedIcon />{' '}
-                        As with good docs, a complete description is always appreciated.
-                    </S.FormLabel>
-                </S.FormControl>
-                <S.FormControl sx={{ flexDirection: 'row' }}>
-                    <S.FormLabel>
-                        Which programming language are you using?
-                    </S.FormLabel>
-                    <SelectUnstyled
-                    components={{ 
-                        Root: SelectRoot, 
-                        Listbox: SelectListbox, 
-                        Popper: SelectPopper
-                    }}
-                    value={props.language}
-                    onChange={props.onLanguageChange}>
-                        {Object.values(ProgrammingLanguage).map(language =>
-                            <SelectOption key={uuid()} value={language}>
-                                {language}
-                                <S.LanguageIcon className={getLanguageIcon(language)} />
-                            </SelectOption>
-                        )}
-                    </SelectUnstyled>
-                </S.FormControl>
-            </S.FieldsContainer>
-            <S.EditorContainer elevation={2}>
-                <AceEditor 
-                name="PARADIGM_TEXT_EDITOR"
-                mode={props.language}
-                theme="github"
-                fontSize="14px"
-                width="100%"
-                minLines={EDITOR_NUM_LINES}
-                maxLines={EDITOR_NUM_LINES}
-                showPrintMargin={false}
-                value={props.content}
-                onChange={(value, _) => props.onContentChange(value)}
-                setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true
-                }} />
-            </S.EditorContainer>
-        </S.Slide>
-        <S.Button 
-        disabled={props.name.length === 0 || props.description.length === 0 || props.content.length === 0} 
-        onClick={props.onContinue}>
-            Continue
-        </S.Button>
-    </>
-);
+const EditorSlide: FC<EditorSlideProps> = (props) => {
+    const canContinue = process.env['NEXT_PUBLIC_VERCEL_ENV']! === 'development' || (
+        props.name.length > 0 && props.description.length > 0 && props.content.length > 0
+    );
+
+    return (
+        <>
+            <S.Slide>
+                <S.FieldsContainer>
+                    <S.FormControl>
+                        <Input
+                        value={props.name}
+                        onChange={(event) => props.onNameChange(event.target.value)}
+                        placeholder="Baptize your snippet."
+                        required />
+                        <S.FormLabel sx={{ textIndent: '0.5rem' }}>
+                            <TungstenOutlinedIcon />{' '}
+                            Name it like a variable: Keep it short, but self-explanatory.
+                        </S.FormLabel>
+                    </S.FormControl>
+                    <S.FormControl>
+                        <Input
+                        value={props.description}
+                        onChange={event => props.onDescriptionChange(event.target.value)}
+                        placeholder="What is your snippet about?"
+                        multiline
+                        rows={3} />
+                        <S.FormLabel sx={{ textIndent: '0.5rem' }}>
+                            <TungstenOutlinedIcon />{' '}
+                            As with good docs, a complete description is always appreciated.
+                        </S.FormLabel>
+                    </S.FormControl>
+                    <S.FormControl sx={{ flexDirection: 'row' }}>
+                        <S.FormLabel>
+                            Which programming language are you using?
+                        </S.FormLabel>
+                        <SelectUnstyled
+                        components={{ 
+                            Root: SelectRoot, 
+                            Listbox: SelectListbox, 
+                            Popper: SelectPopper
+                        }}
+                        value={props.language}
+                        onChange={props.onLanguageChange}>
+                            {Object.values(ProgrammingLanguage).map(language =>
+                                <SelectOption key={uuid()} value={language}>
+                                    {language}
+                                    <S.LanguageIcon className={getLanguageIcon(language)} />
+                                </SelectOption>
+                            )}
+                        </SelectUnstyled>
+                    </S.FormControl>
+                </S.FieldsContainer>
+                <S.EditorContainer elevation={2}>
+                    <AceEditor 
+                    name="PARADIGM_TEXT_EDITOR"
+                    mode={props.language}
+                    theme="github"
+                    fontSize="14px"
+                    width="100%"
+                    minLines={EDITOR_NUM_LINES}
+                    maxLines={EDITOR_NUM_LINES}
+                    showPrintMargin={false}
+                    value={props.content}
+                    onChange={(value, _) => props.onContentChange(value)}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true
+                    }} />
+                </S.EditorContainer>
+            </S.Slide>
+            <S.Button disabled={!canContinue} onClick={props.onContinue}>
+                Continue
+            </S.Button>
+        </>
+    );
+}
 
 export default EditorSlide;
