c1e411020ddf81132af329869efaa297b705cfee
Maria José Solano <majosolano99@gmail.com>
2022-08-21 21:31:02 -0700

    Simplify nav breadcrumbs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,15 @@
 import createEmotionCache from 'styles/emotion-cache';
 import theme from 'styles/theme';
-import routes from 'routes';
 import type { AppEmotionProps } from 'next/app';
-import type { BreadcrumbsProps } from 'components/Breadcrumbs';
 
 import Head from 'next/head';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
-import Breadcrumbs from 'components/Breadcrumbs';
+import NavigationBreadcrumbs from 'components/NavigationBreadcrumbs';
 import ParadigmTitle from 'components/ParadigmTitle';
 
 const clientCache = createEmotionCache();
-
-const navigationLinks: BreadcrumbsProps['links'] = [
-    { ...routes.home, title: 'Home' },
-    { ...routes.searchSnippets, title: 'Search' },
-    { ...routes.newSnippet, title: 'New snippet' }
-];
 
 // TODO: Add Head component
 const App = (props: AppEmotionProps) => {
@@ -32,7 +24,7 @@ const App = (props: AppEmotionProps) => {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <ParadigmTitle />
-                <Breadcrumbs links={navigationLinks} />
+                <NavigationBreadcrumbs />
                 <Component { ...pageProps } />
             </ThemeProvider>
         </CacheProvider>
