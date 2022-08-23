000105b67f04b01f3376b8ce879e30b38bfa3ec9
Maria José Solano <majosolano99@gmail.com>
2022-08-22 22:32:19 -0700

    Deploy script nits

--- scripts/deploy-to-vercel.js
+++ scripts/deploy-to-vercel.js
@@ -1,15 +1,15 @@
 // @ts-check
 
-// Utility script for deploying to Vercel when the git commit message
+// Utility script for deploying to Vercel when the last git commit message
 // contains 'deploy to vercel' (ignoring case) as a substring.
 
 const triggerMessage = 'deploy to vercel';
 const commitMessage = process.env['VERCEL_GIT_COMMIT_MESSAGE'];
 
 if (commitMessage?.toLowerCase().includes(triggerMessage)) {
-    console.log('Vercel deployment triggered.');
+    console.log('[deploy-to-vercel.js] Vercel deployment triggered.');
     process.exit(1);
 } else {
-    console.log('Vercel deployment skipped.');
+    console.log('[deploy-to-vercel.js] Vercel deployment skipped.');
     process.exit(0);
 }
