759db6f92c4f3f61ef588568072632ea3feea9a2
Maria José Solano <majosolano99@gmail.com>
2022-08-28 14:33:10 -0700

    Fetch initial snippets

--- pages/snippet/search.tsx
+++ pages/snippet/search.tsx
@@ -1,23 +1,39 @@
 import { useState } from 'react';
 import useDebounce from 'hooks/useDebounce';
 import { getHookedSdk } from 'lib/graphql';
-import { ProgrammingLanguage } from 'graphql-server/sdk';
-import type { NextPage } from 'next';
+import type { ProgrammingLanguage } from 'graphql-server/sdk';
+import type { GetSnippetsQuery } from 'graphql-server/sdk';
+import type { NextPage, GetStaticProps } from 'next';
 
 import SearchBar from 'components/search-snippets/SearchBar';
 import SnippetsMasonry from 'components/search-snippets/SnippetsMasonry';
 
+type SearchSnippetsPageProps = {
+    initialSnippets: GetSnippetsQuery;
+}
+
 const sdk = getHookedSdk();
 
-const SearchSnippetsPage: NextPage = () => {
+export const getStaticProps: GetStaticProps<SearchSnippetsPageProps> = async () => {
+    // Get 50 random snippets to display initially.
+    const initialSnippets = await sdk.getSnippets({});
+    initialSnippets.snippets = initialSnippets.snippets.slice(0, 50);
+
+    return {
+        props: { initialSnippets },
+        revalidate: 60 * 60 // Re-generate the page every hour
+    }
+}
+
+const SearchSnippetsPage: NextPage<SearchSnippetsPageProps> = (props) => {
     const [query, setQuery] = useState('');
-    const [languageFilter, setLanguageFilter] = useState<ProgrammingLanguage[]>([ ProgrammingLanguage.Javascript ]);
-    const debouncedQuery = useDebounce(query, 3 * 1000);
+    const [languageFilter, setLanguageFilter] = useState<ProgrammingLanguage[]>([]);
+    const debouncedQuery = useDebounce(query, 1000);
 
     const { data } = sdk.useGetSnippets(['getSnippets', debouncedQuery, languageFilter], {
-        query: debouncedQuery
-        // languages: languageFilter.length > 0 ? languageFilter : undefined
-    });
+        query: debouncedQuery,
+        languages: languageFilter.length > 0 ? languageFilter : undefined
+    }, { fallbackData: props.initialSnippets });
 
     return (
         <>
@@ -26,7 +42,7 @@ const SearchSnippetsPage: NextPage = () => {
             onQueryChange={query => setQuery(query)}
             languageFilter={languageFilter}
             onLanguageFilterChange={filter => setLanguageFilter(filter)} />
-            {data && <SnippetsMasonry snippets={data.snippets} />}
+            <SnippetsMasonry snippets={data!.snippets} />
         </>
     );
 }
