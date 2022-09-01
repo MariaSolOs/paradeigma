25c93936caef4d1c980b19b4083884ed25d8cbcd
Maria José Solano <majosolano99@gmail.com>
2022-08-31 22:35:55 -0700

    Urls fixes and deploy to Vercel

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -7,10 +7,8 @@ import { getSdkWithHooks } from 'graphql-server/sdk';
 export const getHookedSdk = () => {
     // The root URL depends on whether we're calling this from the browser
     // or the backend.
-    const env = (process.env['VERCEL_ENV'] || process.env['NEXT_PUBLIC_VERCEL_ENV'])!;
-    const protocolPrefix = env === 'development' ? 'http://' : 'https://';
-    const vercelUrl = (process.env['VERCEL_URL'] || process.env['NEXT_PUBLIC_VERCEL_URL'])!;
+    const rootUrl = (process.env['PARADEIGMA_URL'] || process.env['NEXT_PUBLIC_PARADEIGMA_URL'])!;
 
-    const graphQLClient = new GraphQLClient(`${protocolPrefix}${vercelUrl}/api/graphql`);
+    const graphQLClient = new GraphQLClient(`${rootUrl}/api/graphql`);
     return getSdkWithHooks(graphQLClient);
 }
