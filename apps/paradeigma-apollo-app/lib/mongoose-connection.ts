53ac6d8a031ff3897bccc35762cc7884392d0b33
Maria José Solano <majosolano99@gmail.com>
2022-09-05 15:29:00 -0700

    Make the shared project work

---
+++ apps/paradeigma-apollo-app/lib/mongoose-connection.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import type { Mongoose } from 'mongoose';
+
+/**
+ * Module-scoped mongoose connection promise, so that we can share it
+ * across different functions.
+ */
+let mongooseConnection: Promise<Mongoose>;
+
+const getMongoosePromise = () => mongoose.connect(process.env['MONGODB_URI']!).then(mongoose => {
+    console.log('Mongoose connected.');
+    return mongoose;
+});
+
+if (process.env['VERCEL_ENV']! === 'development') {
+    // In development, use a global variable so that the value is preserved 
+    // across module reloads caused by HMR (Hot Module Replacement).
+    if (!global.mongoosePromise) {
+        global.mongoosePromise = getMongoosePromise();
+    }
+
+    mongooseConnection = global.mongoosePromise;
+} else {
+    // In production mode, it's best to not use a global variable.
+    mongooseConnection = getMongoosePromise();
+}
+
+export default mongooseConnection;
