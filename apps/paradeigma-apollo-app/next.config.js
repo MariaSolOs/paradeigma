b852f662f152fc7cfa002e2d571573d0d3cb8d8f
Maria José Solano <majosolano99@gmail.com>
2022-09-07 19:48:56 -0700

    Add apollo redirect

--- apps/paradeigma-apollo-app/next.config.js
+++ apps/paradeigma-apollo-app/next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
     swcMinify: true,
     experimental: {
         externalDir: true
+    },
+    async redirects() {
+        return [
+            {
+                // Redirect all requests to /api/graphql
+                source: '/:path((?!api/graphql$).*)*',
+                destination: '/api/graphql',
+                permanent: true
+            }
+        ];
     }
 }
 
