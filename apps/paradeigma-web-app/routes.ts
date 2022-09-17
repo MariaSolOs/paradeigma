37f23ee78daafbfc4beb72bee74c067810d906fb
Maria José Solano <majosolano99@gmail.com>
2022-09-17 13:56:36 -0700

    Fix route param name

--- apps/paradeigma-web-app/routes.ts
+++ apps/paradeigma-web-app/routes.ts
@@ -4,7 +4,7 @@ const routes = {
     home: createRoute('/'),
     searchMikros: createRoute('/mikro/search'),
     newMikro: createRoute('/mikro/new'),
-    mikro: (id: string) => createRoute('/mikro/[id]', { mikroId: id })
+    mikro: (id: string) => createRoute('/mikro/[id]', { id })
 } as const;
 
 export default routes;
