95219ca68575728ea44496bdc286418cecd8ee01
Maria José Solano <majosolano99@gmail.com>
2022-09-24 20:09:07 -0700

    Type errors

--- packages/paradeigma-graphql/src/constants.ts
+++ packages/paradeigma-graphql/src/constants.ts
@@ -1,3 +1,5 @@
+import { ProgrammingLanguage, MikroStyle } from './sdk';
+
 /**
  * Maximum length for a mikro's name field.
  */
@@ -7,3 +9,7 @@ export const MIKRO_NAME_MAX_LENGTH = 30;
  * Maximum length for a mikro's description field.
  */
 export const MIKRO_DESCRIPTION_MAX_LENGTH = 100;
+
+export const ProgrammingLanguages = Object.values(ProgrammingLanguage);
+
+export const MikroStyles = Object.values(MikroStyle);
--- packages/paradeigma-graphql/src/index.ts
+++ packages/paradeigma-graphql/src/index.ts
@@ -1,9 +1,4 @@
-import { ProgrammingLanguage, MikroStyle } from './sdk';
-
-export const ProgrammingLanguages = Object.values(ProgrammingLanguage);
-export const MikroStyles = Object.values(MikroStyle);
-
-export * from './sdk'; 
+export * from './sdk';
 export * from './constants';
 export { default as typeDefs } from './schema';
 export type { Resolvers } from './resolvers-types';
