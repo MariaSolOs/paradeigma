169b0e0e14655091df0346bd8d69eeee1f048b54
Maria José Solano <majosolano99@gmail.com>
2022-10-01 18:38:05 -0700

    Shared build script

---
+++ scripts/ignore-vercel-build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+if [[ $1 == "apollo-app" ]]; then
+    triggers=(
+        '../apps/paradeigma-apollo-app'
+        '../packages/paradeigma-graphql'
+        '../packages/paradeigma-mongoose'
+    )
+elif [[ $1 == "web-app" ]]; then
+    triggers=(
+        '../apps/paradeigma-apollo-app'
+        '../packages/paradeigma-graphql'
+    )
+else
+    echo "Invalid app name."
+    exit 0
+fi
+
+for package in ${triggers[@]}; do
+    git diff --quiet $VERCEL_GIT_PREVIOUS_SHA HEAD -- $package
+    exit_code=$?
+    echo "Changes in $package: $exit_code"
+    if [[ $exit_code -eq 1 ]]; then
+        echo "Deployment triggered"
+        exit 1
+    fi
+done
+
+echo "No changes in trigger packages"
+exit 0
