9742c9b13ae6655d3ae4ffdfb6a3b84273e138d2
Maria José Solano <majosolano99@gmail.com>
2022-10-11 18:35:18 -0700

    Add security headers

--- apps/paradeigma-apollo-app/next.config.mjs
+++ apps/paradeigma-apollo-app/next.config.mjs
@@ -1,5 +1,5 @@
 /**
- * @type {import('next').NextConfig} 
+ * @type {import('next').NextConfig}
  */
 export default {
     reactStrictMode: true,
@@ -8,15 +8,38 @@ export default {
         externalDir: true
     },
     // eslint-disable-next-line @typescript-eslint/require-await
+    async headers() {
+        return [{
+            source: '/:path*',
+            headers: [
+                {
+                    key: 'Strict-Transport-Security',
+                    // max-age is set to 2 years (Vercel's default)
+                    value: 'max-age=63072000; includeSubDomains; preload'
+                },
+                {
+                    key: 'Referrer-Policy',
+                    value: 'strict-origin-when-cross-origin'
+                },
+                {
+                    key: 'X-Content-Type-Options',
+                    value: 'nosniff'
+                },
+                {
+                    key: 'Content-Security-Policy',
+                    value: "default-src 'self'"
+                }
+            ]
+        }];
+    },
+    // eslint-disable-next-line @typescript-eslint/require-await
     async redirects() {
-        return [
-            {
-                // Redirect all requests to /api/graphql, except for the GraphQL 
-                // endpoint itself and the task API routes.
-                source: '/:path((?!api/graphql$|api/tasks/.*).*)',
-                destination: '/api/graphql',
-                permanent: true
-            }
-        ];
+        return [{
+            // Redirect all requests to /api/graphql, except for the GraphQL
+            // endpoint itself and the task API routes.
+            source: '/:path((?!api/graphql$|api/tasks/.*).*)',
+            destination: '/api/graphql',
+            permanent: true
+        }];
     }
 }
--- apps/paradeigma-web-app/next.config.mjs
+++ apps/paradeigma-web-app/next.config.mjs
@@ -1,13 +1,41 @@
 import withRoutes from 'nextjs-routes/config';
 
 /** 
- * @type {import('next').NextConfig} 
+ * @type {import('next').NextConfig}
  */
 const config = {
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
         externalDir: true
+    },
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async headers() {
+        return [{
+            source: '/:path*',
+            headers: [
+                {
+                    key: 'Strict-Transport-Security',
+                    // max-age is set to 2 years (Vercel's default)
+                    value: 'max-age=63072000; includeSubDomains; preload'
+                },
+                {
+                    key: 'Referrer-Policy',
+                    value: 'strict-origin-when-cross-origin'
+                },
+                {
+                    key: 'X-Content-Type-Options',
+                    value: 'nosniff'
+                },
+                {
+                    key: 'Content-Security-Policy',
+                    value: `
+                        default-src 'self' https://paradeigma-web-app*.vercel.app;
+                        font-src 'self' https://fonts.google.com;
+                    `.replace(/\s{2,}/g, ' ').trim() // Replace new lines by spaces
+                }
+            ]
+        }];
     }
 }
 
