0e73b8e922e88e8815526cb0392a4bbdaac3484a
Maria José Solano <majosolano99@gmail.com>
2022-10-30 13:07:56 -0700

    Format d.ts files

--- packages/paradeigma-mongoose/types/mongoose.d.ts
+++ packages/paradeigma-mongoose/types/mongoose.d.ts
@@ -1,7 +1,5 @@
 declare module 'mongoose' {
     // Similar to SchemaDefinition but requires every key in T to
     // also be defined in the schema.
-    type SchemaDefinition<T> = Omit<{
-        [key in keyof T]: SchemaDefinitionProperty<T[key]>;
-    }, '_id'>;
+    type SchemaDefinition<T> = Omit<{ [key in keyof T]: SchemaDefinitionProperty<T[key]> }, '_id'>;
 }
