ffbecc1a6bc8e0627e400f8dda586880f32501de
Maria José Solano <majosolano99@gmail.com>
2022-11-08 19:44:27 -0800

    TS comments

--- packages/paradeigma-mongoose/types/mongoose.d.ts
+++ packages/paradeigma-mongoose/types/mongoose.d.ts
@@ -1,5 +1,6 @@
 declare module 'mongoose' {
-    // Similar to SchemaDefinition but requires every key in T to
-    // also be defined in the schema.
+    // Make mongoose's definition more strict by requiring every key in
+    // T to also be defined in the schema (expect for the ID, which is
+    // added automatically).
     type SchemaDefinition<T> = Omit<{ [key in keyof T]: SchemaDefinitionProperty<T[key]> }, '_id'>;
 }
