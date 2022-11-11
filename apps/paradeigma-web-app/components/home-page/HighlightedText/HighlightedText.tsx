82e9f0f087b742d322357d77418134bd8134d482
Maria José Solano <majosolano99@gmail.com>
2022-11-10 18:46:04 -0800

    Open tooltips with touch

--- apps/paradeigma-web-app/components/home-page/HighlightedText/HighlightedText.tsx
+++ apps/paradeigma-web-app/components/home-page/HighlightedText/HighlightedText.tsx
@@ -1,3 +1,6 @@
+import { useMantineTheme } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import { getMaxWidthMediaQuery } from 'lib/styles';
 import useStyles from './HighlightedText.styles';
 import type { FCC } from 'react';
 import type { HighlightedTextProps } from './index';
@@ -7,6 +10,9 @@ import { Text, Tooltip } from '@mantine/core';
 const HighlightedText: FCC<HighlightedTextProps> = (props) => {
     const { classes } = useStyles();
 
+    const theme = useMantineTheme();
+    const isSmallScreen = useMediaQuery(getMaxWidthMediaQuery(theme.breakpoints.xs));
+
     if (props.tooltipText) {
         return (
             <Tooltip
@@ -14,6 +20,12 @@ const HighlightedText: FCC<HighlightedTextProps> = (props) => {
                 transition="pop"
                 transitionDuration={200}
                 label={props.tooltipText}
+                events={{
+                    hover: true,
+                    focus: false,
+                    // On mobile, open the tooltip with touch.
+                    touch: isSmallScreen
+                }}
                 classNames={{ tooltip: classes.tooltip }}>
                 <Text component="span" className={classes.bungee}>
                     {props.children}
