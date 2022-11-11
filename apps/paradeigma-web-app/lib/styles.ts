4048c10ff5096fd295a938c04e88ac5774269e4c
Maria José Solano <majosolano99@gmail.com>
2022-11-10 18:45:47 -0800

    Move media query helper

--- apps/paradeigma-web-app/lib/styles.ts
+++ apps/paradeigma-web-app/lib/styles.ts
@@ -4,3 +4,9 @@ import { createEmotionCache } from '@mantine/core';
  * Style cache, shared between the server and client.
  */
 export const cache = createEmotionCache({ key: 'paradeigma-styles' });
+
+/**
+ * @param breakpoint - Width (in pixels) for generating the media query.
+ * @returns The CSS media query string.
+ */
+export const getMaxWidthMediaQuery = (breakpoint: number) => `(max-width:${breakpoint}px)`;
